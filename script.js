a2ee1c2f011c12793f587a0f95cdb70c4fc8736f
Kim Jones <Kim_Jones5939@aol.com>
2026-04-11 14:51:33 -0400

    update

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-!
+document.querySelectorAll(" .toggle").forEach(button => {
+    button.addEventListener("click", () => {
+        const checklist = button.nextElementSibling;
+        checklist.classList.toggle("hidden");
+    });
+});
+
+document.querySelectorAll("a[href^='#']").forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute("href"));
+        if (target) {
+            target.scrollIntoView({ behavior: "smooth" });
+        }
+    });
+});
